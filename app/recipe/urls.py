c1e17d57302f2c284c42c7b77e22443b52492da9
Gerson Fernando Budke <nandojve@gmail.com>
2019-06-20 19:36:32 -0300

    Add list recipes feature
    Signed-off-by: Gerson Fernando Budke <nandojve@gmail.com>

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -5,9 +5,11 @@ from recipe import views
 
 
 app_name = 'recipe'
+
 router = DefaultRouter()
 router.register('tags', views.TagViewSet)
-router.register('ingredient', views.IngredientViewSet)
+router.register('ingredients', views.IngredientViewSet)
+router.register('recipes', views.RecipeViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
