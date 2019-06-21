044a1f447d7f5ac3cb7d5bb7003a79c9adedb143
Gerson Fernando Budke <nandojve@gmail.com>
2019-06-20 22:51:21 -0300

    Add image field to recipe model
    Signed-off-by: Gerson Fernando Budke <nandojve@gmail.com>

--- Dockerfile
+++ Dockerfile
@@ -35,5 +35,10 @@ RUN mkdir /app
 WORKDIR /app
 COPY ./app /app
 
+RUN mkdir -p /vol/web/media
+RUN mkdir -p /vol/web/static
+
 RUN useradd -ms /bin/bash user
+RUN chown -R user:user /vol/
+RUN chmod -R 755 /vol/web
 USER user
--- app/app/urls.py
+++ app/app/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 from rest_framework_swagger.views import get_swagger_view
 
 schema_view = get_swagger_view(title='User API')
@@ -24,4 +27,4 @@ urlpatterns = [
     path('api/user/', include('user.urls')),
     path('api/recipe/', include('recipe.urls')),
     path('api/', schema_view),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
